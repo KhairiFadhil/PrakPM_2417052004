fbe4ac77adc7c9de62c7e5566397e65c1627ad02
Kalri <53751770+KhairiFadhil@users.noreply.github.com>
2026-03-02 14:46:58 +0700

    Implementasi Gambar

--- app/src/main/java/com/example/prakpm_2417052004/MainActivity.kt
+++ app/src/main/java/com/example/prakpm_2417052004/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.prakpm_2417052004.ui.theme.PrakPM_2417052004Theme
+import model.Food
+import model.FoodSource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,10 +31,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             PrakPM_2417052004Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding),
-                        name = "Halo, saya Raid Fadhil Khairi dengan NPM 2417052004 siap belajar Compose!"
-                    )
+                    FoodScreen(foods = FoodSource.dummyFood,
+                                modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +40,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FoodScreen(foods: List<Food>, modifier: Modifier = Modifier){
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp)
+    ){
+        Text(
+            text = "Selamat Datang, User!",
+            style = MaterialTheme.typography.titleMedium,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Mau makan apa hari ini?",
+            style = MaterialTheme.typography.titleSmall,
+            fontWeight = FontWeight.Normal
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Column(){
+            foods.forEach{ food ->
+                FoodCard(food)
+            }
+        }
+    }
+}
+
+@Composable
+fun FoodCard(food : Food){
+    Column(){
+        Text(text = "Tipe Makanan: ${food.nama}")
+        Text(text = "Jenis: ${food.deskripsi}")
+        Text(text = "Harga : Rp.${food.harga}")
+        Image(
+            painter = painterResource(id = food.imageRes),
+            contentDescription = food.deskripsi,
+            modifier = Modifier.size(72.dp)
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     PrakPM_2417052004Theme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/model/FoodSource.kt
@@ -0,0 +1,13 @@
+package model
+
+import com.example.prakpm_2417052004.R
+
+object FoodSource {
+    val dummyFood = listOf(
+        Food("Sate", "Ayam", 10000, R.drawable.sate_ayam),
+        Food("Sate", "Padang", 250000, R.drawable.sate_padang),
+        Food("Ayam", "Geprek", 10000, R.drawable.ayam_geprek),
+        Food("Ayam", "Bakar", 12000, R.drawable.ayam_bakar),
+        Food("Sate", "Taichan", 15000, R.drawable. sate_taichan)
+    )
+}
